f4724862be945e94bbfa514b9def844fc354ad38
Dennis Muga <dennismuga22@gmail.com>
2025-02-25 07:46:20 +0300

    bug fix

--- utils/corner_stone.py
+++ utils/corner_stone.py
@@ -41,18 +41,7 @@ class CornerStone():
                     for odd in odds:
                         odd_value = float(odd.get('odd_value'))           
                         bet_pick = odd.get('odd_key')  
-                        outcome_id = None   
-                        # special_bet_value = odd.get('special_bet_value')      
-                        # outcome_id = odd.get('outcome_id')
-                        # if (odd_value>=1.29 and odd_value<=1.34) or (odd_value>=1.43 and odd_value <= 1.44) and 'over' in bet_pick:   
-                        if (odd_value < 1.34 and bet_pick == 'over 7.5'):    
-                            for odd in odds:    
-                                overall_prob = 98 if odd_value < 1.3 else 88
-                                bet_pick = odd.get('odd_key')   
-                                if bet_pick == 'over 8.5':   
-                                    odd_value = float(odd.get('odd_value'))    
-                                    special_bet_value = odd.get('special_bet_value')      
-                                    outcome_id = odd.get('outcome_id')
+
                         if 'under' in bet_pick:
                             under = float(bet_pick.replace('under ', ''))
                             for odd in odds:    
@@ -62,24 +51,22 @@ class CornerStone():
                                     special_bet_value = odd.get('special_bet_value')      
                                     outcome_id = odd.get('outcome_id')
                                     overall_prob = 98 if odd_value < 1.35 else 88
-     
-                        if outcome_id:
-                            match = {
-                                'match_id': match_id,
-                                'start_time': start_time,
-                                'home_team': home_team,
-                                'away_team': away_team,
-                                'prediction': bet_pick,
-                                'odd': odd_value,
-                                'overall_prob': overall_prob,
-                                'parent_match_id': parent_match_id,
-                                'sub_type_id': sub_type_id,
-                                'bet_pick': bet_pick,
-                                'special_bet_value': special_bet_value,
-                                'outcome_id': outcome_id
-                            }
-                            print(match)
-                            self.db.insert_match(match)
+                                    match = {
+                                        'match_id': match_id,
+                                        'start_time': start_time,
+                                        'home_team': home_team,
+                                        'away_team': away_team,
+                                        'prediction': bet_pick,
+                                        'odd': odd_value,
+                                        'overall_prob': overall_prob,
+                                        'parent_match_id': parent_match_id,
+                                        'sub_type_id': sub_type_id,
+                                        'bet_pick': bet_pick,
+                                        'special_bet_value': special_bet_value,
+                                        'outcome_id': outcome_id
+                                    }
+                                    print(match)
+                                    self.db.insert_match(match)
 
     def __call__(self):
         # Use ThreadPoolExecutor to spawn a thread for each match
