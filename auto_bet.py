2810055a8cb0dbcd4500a9d73533aa81068c4eec
Dennis Muga <dennismuga22@gmail.com>
2025-02-26 02:05:59 +0300

    all

--- auto_bet.py
+++ auto_bet.py
@@ -23,7 +23,7 @@ class AutoBet():
     
     def auto_bet(self):
         betslips = []
-        max_games = 10
+        max_games = 11
         total_odd = 1
         composite_betslip = None
         composite_betslips = []
