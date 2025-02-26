853a413962eec678d82da88074a2715ded4586a4
Dennis Muga <dennismuga22@gmail.com>
2025-02-26 09:44:56 +0300

    all

--- app.py
+++ app.py
@@ -71,16 +71,16 @@ def home():
         return subscribe()
     
     else:
-        matches = helper.fetch_matches('', '=', '', limit=15)
+        matches = helper.fetch_matches('', '=', '', limit=16)
         lm = len(matches)
-        matches_yesterday = helper.fetch_matches('-1', '=', '', limit=15)
+        matches_yesterday = helper.fetch_matches('-1', '=', '', limit=16)
         lmy = len(matches_yesterday)
         plans = [
-            Plan('Free Tips', 0, 2, 'pink', 1, matches[lm-4:lm] if lm>= 4 else matches, matches_yesterday[lmy-4:lmy] if lmy>= 5 else matches_yesterday),
-            Plan('Bronze Plan', 20, 5, 'purple', 2, matches[:5] if lm>= 5 else matches, matches_yesterday[:5] if lmy>= 5 else matches_yesterday),
-            Plan('Silver Plan', 30, 8, 'blue', 3, matches[:7] if lm>= 7 else matches, matches_yesterday[:7] if lmy>= 7 else matches_yesterday),
-            Plan('Gold Plan', 50, 12, 'yellow', 4, matches[:9] if lm>= 9 else matches, matches_yesterday[:9] if lmy>= 9 else matches_yesterday),
-            Plan('Platinum Plan', 70, 18, 'green', 5, matches[:11] if lm>= 11 else matches, matches_yesterday[:11] if lmy>= 11 else matches_yesterday)
+            Plan('Free Tips', 0, 2, 'pink', 1, matches[lm-4:lm] if lm>= 4 else matches, matches_yesterday[lmy-4:lmy] if lmy>= 4 else matches_yesterday),
+            Plan('Bronze Plan', 20, 5, 'purple', 2, matches[:6] if lm>= 6 else matches, matches_yesterday[:6] if lmy>= 6 else matches_yesterday),
+            Plan('Silver Plan', 30, 8, 'blue', 3, matches[:8] if lm>= 8 else matches, matches_yesterday[:8] if lmy>= 8 else matches_yesterday),
+            Plan('Gold Plan', 50, 12, 'yellow', 4, matches[:10] if lm>= 10 else matches, matches_yesterday[:10] if lmy>= 10 else matches_yesterday),
+            Plan('Platinum Plan', 70, 18, 'green', 5, matches[:12] if lm>= 12 else matches, matches_yesterday[:12] if lmy>= 12 else matches_yesterday)
         ]
         return render_template('home.html', plans=plans)
 
