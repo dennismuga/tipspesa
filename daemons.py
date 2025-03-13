aa3e5cbbd49f53b0b6d65a8e658fbe8d14715638
dennismuga <106347773+dennismuga@users.noreply.github.com>
2025-03-13 22:54:49 +0300

    Update daemons.py

--- daemons.py
+++ daemons.py
@@ -5,21 +5,19 @@ from v2.predict_and_bet import PredictAndBet
 from v2.stats import Stats
     
 def update_stats():
+    i = 0
     while True:
         Stats()()
+        if i>0 and i%2 == 0:
+           PredictAndBet()()
         time.sleep(120)
-
-def predict_and_bet():
-    while True:
-        PredictAndBet()()
-        time.sleep(300)
 
 if __name__ == '__main__':
     # Use ThreadPoolExecutor to spawn a thread for each match
     with concurrent.futures.ThreadPoolExecutor() as executor:
         threads = [
-            executor.submit(update_stats),
-            executor.submit(predict_and_bet)
+            executor.submit(update_stats)
+            #executor.submit(predict_and_bet)
         ]
 
         # Wait for all threads to finish
