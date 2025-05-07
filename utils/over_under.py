b6a363765082ecbdacf1658f48a427e55fdb11a1
Dennis Muga <dennis.mutethia1@accesscfa.com>
2025-05-08 01:01:32 +0300

    rm breaks

--- utils/over_under.py
+++ utils/over_under.py
@@ -112,33 +112,32 @@ class OverUnder():
                                         bet_pick = odd.get('odd_key')
                                         odd_value = float(odd.get('odd_value'))   
                                         special_bet_value = odd.get('special_bet_value')
-                                        outcome_id = odd.get('outcome_id')
-                                        #predict under 4.5
+                                        outcome_id = odd.get('outcome_id')                                        
+                                        
                                         if float(odd_2.get('odd_value')) < 1.1:     
                                             for odd_3 in odds:
-                                                if odd_3.get('odd_key') == 'under 4.5':
+                                                if odd_3.get('odd_key') == 'under 4.5': #predict under 4.5
                                                     bet_pick = odd_3.get('odd_key')
                                                     odd_value = float(odd_3.get('odd_value'))   
                                                     special_bet_value = odd_3.get('special_bet_value')
                                                     outcome_id = odd_3.get('outcome_id')
-                                                    break
-                                        break
+                                                    
                                     elif float(odd_2.get('odd_value')) < float(odd.get('odd_value'))-0.2:   #predict under 5.5                                  
                                         bet_pick = odd_2.get('odd_key')
                                         odd_value = float(odd_2.get('odd_value'))   
                                         special_bet_value = odd_2.get('special_bet_value')
                                         outcome_id = odd_2.get('outcome_id')
-                                        break
                                           
                         if odd.get('odd_key') == 'over 2.5' and not bet_pick:
                             for odd_2 in odds:
                                 if odd_2.get('odd_key') == 'under 4.5': #predict over 2.5
-                                    if float(odd_2.get('odd_value')) > float(odd.get('odd_value'))+0.1:                                    
+                                    if float(odd_2.get('odd_value')) > float(odd.get('odd_value'))+0.1:    
+                                        pass                                
                                         # bet_pick = odd.get('odd_key')
                                         # odd_value = float(odd.get('odd_value'))   
                                         # special_bet_value = odd.get('special_bet_value')
                                         # outcome_id = odd.get('outcome_id')   
-                                        break        
+                                        # break        
                                     elif float(odd_2.get('odd_value'))<1.24 and float(odd_2.get('odd_value')) < float(odd.get('odd_value'))-0.2:   #predict under 4.5                                 
                                         # bet_pick = odd_2.get('odd_key')
                                         # odd_value = float(odd_2.get('odd_value'))   
@@ -151,8 +150,6 @@ class OverUnder():
                                                     odd_value = float(odd_3.get('odd_value'))   
                                                     special_bet_value = odd_3.get('special_bet_value')
                                                     outcome_id = odd_3.get('outcome_id')
-                                                    break                                        
-                                        break  
                         
             if bet_pick:
                 match = {
