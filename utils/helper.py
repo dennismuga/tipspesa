a3860606c4544a8eac276d72a54e0b40d5e47bed
Dennis Muga <dennis.mutethia1@accesscfa.com>
2025-05-13 01:38:29 +0300

    Update helper.py

--- utils/helper.py
+++ utils/helper.py
@@ -98,7 +98,7 @@ class Helper():
             composite_betslip = None
             composite_betslips = [] 
             total_odd = 1
-            min_odd = 5.0
+            min_odd = 3.0
             for match in matches:   
                 if not any(betslip["parent_match_id"] == match.get("parent_match_id") for betslip in betslips):
                     betslip = {
