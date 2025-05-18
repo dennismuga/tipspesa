78e7f3620f017ca43d67c3815898809e5a949535
Dennis Muga <dennis.mutethia1@accesscfa.com>
2025-05-18 21:03:49 +0300

    rm ov 2.5 use ov1.5

--- utils/multi_goal_over_under.py
+++ utils/multi_goal_over_under.py
@@ -101,20 +101,21 @@ class MultiGoalOverUnder():
                                             }
                                 else:
                                     if float(over_2_5.get('odd_value')) >= 1.2 and float(over_2_5.get('odd_value')) < float(under_4_5.get('odd_value')):
-                                        match = {
-                                                'parent_match_id': parent_match_id,
-                                                'match_id': meta.get('match_id'),
-                                                'start_time': start_time,
-                                                'home_team': meta.get('home_team'),
-                                                'away_team': meta.get('away_team'),
-                                                'overall_prob': 98,                            
-                                                'sub_type_id': "18",
-                                                'prediction': over_2_5.get('odd_key'),
-                                                'bet_pick': over_2_5.get('odd_key'),
-                                                'odd': float(over_2_5.get('odd_value')),
-                                                'special_bet_value': over_2_5.get('special_bet_value'),
-                                                'outcome_id': over_2_5.get('outcome_id')
-                                            }
+                                        if over_1_5:
+                                            match = {
+                                                    'parent_match_id': parent_match_id,
+                                                    'match_id': meta.get('match_id'),
+                                                    'start_time': start_time,
+                                                    'home_team': meta.get('home_team'),
+                                                    'away_team': meta.get('away_team'),
+                                                    'overall_prob': 98,                            
+                                                    'sub_type_id': "18",
+                                                    'prediction': over_1_5.get('odd_key'),
+                                                    'bet_pick': over_1_5.get('odd_key'),
+                                                    'odd': float(over_1_5.get('odd_value')),
+                                                    'special_bet_value': over_1_5.get('special_bet_value'),
+                                                    'outcome_id': over_1_5.get('outcome_id')
+                                                }
                             
                             if match:   
                                 print(match)
