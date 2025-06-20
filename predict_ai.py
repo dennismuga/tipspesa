89713ef75f74ecc3ecc48dfb2b8b0b500575a60d
Dennis Mutethia Muga <10231071+dennis-mutethia@users.noreply.github.com>
2025-06-20 21:17:03 +0300

    Update predict_ai.py

--- predict_ai.py
+++ predict_ai.py
@@ -99,7 +99,7 @@ class PredictAi:
                 print(response)
                 filtered_match = json.loads(response)
                 filtered_match = None if filtered_match["sub_type_id"] in ["18"] else filtered_match
-                predicted_match = filtered_match if filtered_match["odd"] >=1.1 and filtered_match["overall_prob"]>=75 else None
+                predicted_match = filtered_match if filtered_match["odd"] >=1.15 and filtered_match["overall_prob"]>=65 else None
                 predicted_match = None if ' or ' in predicted_match["bet_pick"] and ' draw ' not in predicted_match["bet_pick"] else predicted_match
                 predicted_match = None if predicted_match["bet_pick"] in ['under 2.5', 'under 3.5', '1x2', 'TOTAL'] else predicted_match
                 # predicted_match = filtered_match if ((filtered_match["sub_type_id"] in [24,29,105,548] or (filtered_match["sub_type_id"]==18)) and filtered_match["overall_prob"]>=75 and 'under ' not in filtered_match["bet_pick"] and ' or ' not in filtered_match["bet_pick"] ) else None
