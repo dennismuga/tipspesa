d7b80467b068a16de3358120dbcf0077847e538e
Dennis Mutethia Muga <dennismuga@gmail.com>
2025-07-22 00:36:41 +0300

    Update predict_ai.py

--- predict_ai.py
+++ predict_ai.py
@@ -108,7 +108,7 @@ class PredictAi:
             if query:
                 response = self.gemini.call_gemini_api(query).replace('```json', '').strip('```')
                 filtered_match = json.loads(response)
-                predicted_match = filtered_match if filtered_match["odd"] >=1.15 and filtered_match["overall_prob"]>=80 else None                
+                predicted_match = filtered_match if filtered_match["odd"] >=1.10 and filtered_match["overall_prob"]>=75 else None                
                 
                 if int(predicted_match["sub_type_id"]) in [105] or int(predicted_match["outcome_id"]) in [10, 11, 13]:
                     predicted_match = None
