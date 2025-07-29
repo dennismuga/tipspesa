d0b5f48cbd8e35c7a99992d2cbee7c0811ed05dd
Dennis Mutethia Muga <10231071+dennis-mutethia@users.noreply.github.com>
2025-07-29 13:44:29 +0300

    Update auto_bet.py

--- auto_bet.py
+++ auto_bet.py
@@ -17,7 +17,7 @@ class Autobet:
         profile_id = profile[2]
         matches = self.db.fetch_unplaced_matches(profile_id)
         helper = Helper(phone, password)
-        helper.auto_bet(profile_id, matches, 5)
+        helper.auto_bet(profile_id, matches, 4)
         # helper.auto_bet(profile_id, matches, 8)
         # helper.auto_bet(profile_id, matches, 12)
         # helper.auto_bet(profile_id, matches, 16)
