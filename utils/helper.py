b680dd11911f7ed7f53df8b8d596e2b771a0a193
Dennis Mutethia Muga <10231071+dennis-mutethia@users.noreply.github.com>
2025-08-01 23:02:50 +0300

    Update helper.py

--- utils/helper.py
+++ utils/helper.py
@@ -133,6 +133,7 @@ class Helper():
             if len(composite_betslips) > 0:              
                 usable = self.betika.balance #+ self.betika.bonus
                 stake = int((usable/len(composite_betslips))/2)
+                stake = max(5, stake)
                 stake = 1 if (stake == 0 and int(usable)>0) else stake
                 if stake > 0:
                     composite_betslips.sort(key=lambda cb: cb['total_odd'], reverse=True)
@@ -188,3 +189,4 @@ class Helper():
         return code
     
     
+
