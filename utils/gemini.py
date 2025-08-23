3b3c27b1ef5031e79f745df211e2a99b547577ed
Dennis Mutethia Muga <dennismuga@gmail.com>
2025-08-23 22:01:29 +0300

    Update gemini.py

--- utils/gemini.py
+++ utils/gemini.py
@@ -11,7 +11,7 @@ class Gemini():
     def get_response(self, query):
         try:
             response = self.client.models.generate_content(
-                model= os.getenv('GEMINI_MODEL'),
+                model= "gemini-2.5-flash-lite", # os.getenv('GEMINI_MODEL'), 
                 contents=str(query)
             )
             return response.text
