66229e39e8d2ec133af9ec47f68d80ea94fb64a7
Dennis Mutethia Muga <dennismuga@gmail.com>
2025-10-03 18:50:24 +0300

    Update auto_bet.py

--- auto_bet.py
+++ auto_bet.py
@@ -20,8 +20,8 @@ class Autobet:
         #helper.auto_bet(profile_id, matches, 1)
         # helper.auto_bet(profile_id, matches, 2)
         #helper.auto_bet(profile_id, matches, 3)
-        helper.auto_bet(profile_id, matches, 4)
-        #helper.auto_bet(profile_id, matches, 5)
+        #helper.auto_bet(profile_id, matches, 4)
+        helper.auto_bet(profile_id, matches, 5)
                     
     def __call__(self):
         # Use ThreadPoolExecutor to spawn a thread for each profile
