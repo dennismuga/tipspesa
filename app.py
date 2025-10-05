6d1bba2f9d7183e0598866a7e5633ed406c5ed7f
Dennis Mutethia Muga <dennismuga@gmail.com>
2025-10-05 22:27:50 +0300

    specific models

--- app.py
+++ app.py
@@ -117,7 +117,7 @@ def filter_matches(day, match_count, end_index, status=''):
     return to_return
 
 def get_matches(count, end_index):
-    total = 24 #min_matches.free+min_matches.bronze+min_matches.silver+min_matches.gold+min_matches.platinum
+    total = 28 #min_matches.free+min_matches.bronze+min_matches.silver+min_matches.gold+min_matches.platinum
     five_days_ago = filter_matches('-5', total, total)
     four_days_ago = filter_matches('-4', total, total)
     three_days_ago = filter_matches('-3', total, total)
